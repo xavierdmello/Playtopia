dd22cabd4248194b849b4d1118c420e5b1ad8d46
Xavier D'Mello <xaviermdmello@gmail.com>
2025-01-18 21:20:28 -0500

    word

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import { useTheme } from "./components/theme-provider";
+import { useAccount } from "@starknet-react/core";
 
 function App() {
   const { setTheme } = useTheme();
+
+  const { address, status } = useAccount();
   const [currentPage, setCurrentPage] = useState<"play" | "stake" | "create">(
     "play"
   );
@@ -13,10 +16,19 @@ function App() {
     <div className="container m-auto max-w-5xl mt-[10px] border border-border">
       <Header currentPage={currentPage} setCurrentPage={setCurrentPage} />
       {/* Content Section based on currentPage */}
+
       <div className="p-4">
-        {currentPage === "play" && <div>Play Page</div>}
-        {currentPage === "stake" && <div>Stake Page</div>}
-        {currentPage === "create" && <div>Create Page</div>}
+        {status === "connected" ? (
+          <>
+            {currentPage === "play" && <div>Play Page</div>}
+            {currentPage === "stake" && <div>Stake Page</div>}
+            {currentPage === "create" && <div>Create Page</div>}
+          </>
+        ) : (
+          <div className="m-12 justify-center align-middle flex">
+            Please connect your wallet.
+          </div>
+        )}
       </div>
     </div>
   );
