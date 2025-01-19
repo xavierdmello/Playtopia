4ba3fe59c4a327ed2467ed51b957dbdd82ac94ea
Xavier D'Mello <xaviermdmello@gmail.com>
2025-01-18 20:24:19 -0500

    header buttons

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,17 @@
 import logo from "/Playtopia.png";
-
 import { Button } from "./ui/button";
+
 export default function Header() {
   return (
     <div className="flex flex-row bg-accent justify-between mt-0 border-b p-4 border-border">
       <img src={logo} className="w-40 "></img>
 
+      <div className="flex gap-4">
+        <Button variant="outline">Play</Button>
+        <Button variant="outline">Stake</Button>
+        <Button variant="outline">Create</Button>
+      </div>
+      
       <Button variant="outline">Button</Button>
     </div>
   );
