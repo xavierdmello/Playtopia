8e19004d1aafe317f43fb0c9d1810837aca8b8c1
Xavier D'Mello <xaviermdmello@gmail.com>
2025-01-18 20:52:48 -0500

    a

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -20,12 +20,11 @@ export default function Header() {
 
   return (
     <div className="flex flex-row bg-accent justify-between mt-0 border-b p-4 border-border">
-      <img src={logo} className="w-40 "></img>
-
-      <div className="flex gap-4">
-        <Button variant="outline">Play</Button>
-        <Button variant="outline">Stake</Button>
-        <Button variant="outline">Create</Button>
+      <div className="flex gap-7 items-center ">
+        <img src={logo} className="w-32 "></img>
+        <a>Play</a>
+        <a>Stake</a>
+        <a>Create</a>
       </div>
 
       <Button variant="outline" onClick={connectWallet}>
